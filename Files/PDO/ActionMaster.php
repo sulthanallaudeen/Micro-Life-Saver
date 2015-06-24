99eadc989f5e3a920f8001dde5a62bb3b4f7b006
Sulthan Allaudeen <sulthanallaudeen@users.noreply.github.com>
2015-06-24 13:01:55 +0530

    Update ActionMaster.php
    DB Connection + Table Create + Insert + Select

--- Files/PDO/ActionMaster.php
+++ Files/PDO/ActionMaster.php
@@ -1,3 +1,36 @@
 <?php
-#Initiatilizing with Database Connectivity
+	#Added Credentials
+	$Credentials = "mysql:host=localhost;dbname=test";
+	#Added Exceptions
+	$Options = array(PDO::ATTR_ERRMODE=> PDO::ERRMODE_EXCEPTION,PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC);
+	#Establishing Connection
+	$Connection = new PDO($Credentials,'root','', $Options);
+	#Declaring Table Name
+	$TableName = 'sysaxiom';
+	#Declaring Table Attributes and Creation of Table Creation Query
+	$SQLQuery ="CREATE table $TableName(
+	     ID INT( 11 ) AUTO_INCREMENT PRIMARY KEY,
+	     Name VARCHAR( 250 ) NOT NULL,
+	     Email VARCHAR( 150 ) NOT NULL, 
+	     City VARCHAR( 150 ) NOT NULL)";
+	#Firing the Table Creation Query
+	$Connection->exec($SQLQuery);
+    #Preparing Statement
+    $Trigger = $Connection->prepare("INSERT INTO sysaxiom (Name, Email, City) VALUES (?, ?, ?)");
+    #Binding the Parameter
+	$Trigger->bindParam(1, $Name);
+	$Trigger->bindParam(2, $Email);
+	$Trigger->bindParam(3, $City);
+	$Name = 'Sulthan';
+	$Email = 'sa@sysaxiom.com';
+	$City = 'Chennai';
+	#Executing the Trigger
+	$Trigger->execute();
+	#Selecting the Rows Created so far
+	   foreach($Connection->query('SELECT * from sysaxiom') as $Row) {
+	#Printing the Result
+        print_r($Row);
+    }
+	#Disconnecting the DB Connection
+	$Connection = null;
 ?>
